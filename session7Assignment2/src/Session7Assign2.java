57e37b91a1e0b34a04ccb8ca3a1ff7d55fe44939
VrushaliB25 <vrbhoite@gmail.com>
2017-03-29 21:12:20 +0530

    Session7Assignment2

---
+++ session7Assignment2/src/Session7Assign2.java
@@ -0,0 +1,9 @@
+
+public class Session7Assign2 {
+	public static void main(String s[])
+	{
+		//To write a program to find a substring in a String by using an inbuilt method of String class.
+		String str="example of method substring";
+		System.out.println(""+str.substring(0,7));
+	}
+}
